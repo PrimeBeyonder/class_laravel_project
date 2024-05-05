69da1e167943a3a5c8c71f51e8c8ccbf3485eee4
Andrew <thum14252@gmail.com>
2024-05-05 12:03:32 +0630

    route test

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get("/articles/detail/{content}" , function(){
+    echo "Article Detaile";
+});
+Route::get("/articles" , function(){
+    echo "Article ";
+});
