a796687439ddfc39b9f35c20b8647cb4de3c8b16
Andrew <thum14252@gmail.com>
2024-05-11 12:19:54 +0630

    bootstrap / auth

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,7 @@ Route::get('/', function () {
 Route::get("/articles/detail/{id}" , [ArticleController::class , "detail"]
 );
 Route::get("/articles" , [ArticleController::class , "index"]);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
