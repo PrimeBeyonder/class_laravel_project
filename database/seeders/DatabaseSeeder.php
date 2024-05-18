948d48c538dd2184dfda3b827ed00492577bff99
Andrew <thum14252@gmail.com>
2024-05-18 11:01:34 +0630

    data seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,8 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Article;
+use App\Models\Category;
+use App\Models\Comment;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +17,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Article::factory(25)->create();
+        Comment::factory(60)->create();
+
+        $list = ["News" , "Tech", "Web", "Mobile" ,"OS"];
+        foreach($list as $name ) {
+            Category::create(['name' => $name]);
+        }
 
     }
 }
